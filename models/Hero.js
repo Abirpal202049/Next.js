0231a4a3d5426a6c94a188c3a7e8ddc1168769f2
Abir Pal <abirpal282002@gmail.com>
2022-01-12 11:10:44 +0530

    L2

---
+++ models/Hero.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const HeroSchema = new mongoose.Schema({
+    superhero : {
+        type : String,
+        required : [true, 'Please name the hero'],
+        unique : true,
+        trim : true
+    },
+    realname : {
+        type : String,
+        required : true,
+        maxlength : [200, 'Please keep real name short']
+    }
+})
+
+//? If model is already created it will take that otherwise it will create a new model
+module.exports = mongoose.model.Hero || mongoose.model('Hero', HeroSchema)
--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ function about() {
     return (
         <div>
             <h2>About Page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis molestiae iusto perferendis voluptate incidunt consequuntur soluta! Saepe enim deleniti distinctio ducimus harum, accusamus eligendi doloribus, sit delectus labore rerum laudantium. Repellendus cupiditate labore id accusantium optio sequi corrupti ut possimus, fuga cumque magni adipisci!</p>
         </div>
     )
 }
